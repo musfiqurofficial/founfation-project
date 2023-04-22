f95906007870f4a262cca2d865dfa785c7e67eb1
musfiqurofficial <musfiqurofficial1999@gmail.com>
2023-04-23 04:28:24 +0600

    done

--- src/components/pages/Faq/Faq.jsx
+++ src/components/pages/Faq/Faq.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import FaqHero from './FaqHero';
-import FaqBody from './FaqBody';
+import FaqHero from "./FaqHero";
+import FaqBody from "./FaqBody";
 
 const Faq = () => {
-    return (
-        <div>
-            <FaqHero></FaqHero>
-            <FaqBody></FaqBody>
-        </div>
-    );
+  return (
+    <div>
+      <FaqHero></FaqHero>
+      <div className="bg-[#ffffffe7]">
+        <FaqBody></FaqBody>
+      </div>
+    </div>
+  );
 };
 
 export default Faq;
--- src/components/pages/Faq/FaqHero.jsx
+++ src/components/pages/Faq/FaqHero.jsx
@@ -1,17 +1,16 @@
-import React from "react";
+import React from 'react';
 
 const FaqHero = () => {
-  return (
-    <div className="my-20">
+    return (
+        <div className="lg:my-20 mx-4">
       <div className="px-4 py-5 sm:py-14 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-5 bg-gradient rounded-xl shadow-2xl">
         <div className="flex justify-around items-center text-white">
-          <h1 className="text-[56px] font-bold leading-none">
-            Tour de Sun '22 FAQ
-          </h1>
+          <h1 className="text-[32px] sm:text-[56px] font-bold leading-none">Tour de Sun '22 FAQ</h1>
+        
         </div>
       </div>
     </div>
-  );
+    );
 };
 
 export default FaqHero;
